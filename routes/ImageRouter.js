f0e8118404059f20f6b6ddb903160f98a414ed3f
savinduonline <savinduen@gmail.com>
2024-12-27 09:43:45 +0530

    2nd

--- routes/ImageRouter.js
+++ routes/ImageRouter.js
@@ -1,13 +1,26 @@
 const express = require("express");
 const {readFile} = require("../services/AwsService.js");
 const sharp = require("sharp");
+const sizeOf = require("image-size");
 const router = express.Router();
 
 router.get("/:imageId", async (req,res)=>{
 
+    const width = req.query.s;
+
     const imageByteArray = await readFile("abc.png");
 
+    //get image size
+    var originalImageWidth = 0;
+    var originalImageHeight = 0;
+    const dimentions = await sizeOf(imageByteArray);
+    originalImageWidth = dimentions.width;
+    originalImageHeight = dimentions.height;
+
+    const w = width;
+    const h = originalImageHeight / (originalImageWidth/w);
     const result = await sharp(imageByteArray)
+                    .resize(parseInt(w),parseInt(h))
                     .toBuffer();
 
     res.setHeader("Content-Type","image/png");
